09d48786800c294bde1f5c68e15ba0251387c789
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-10-02 20:01:30 -0500

    functional inputs to table

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 //console.log("js working");
 
+//add global variable to handle table information
+let employeeList = [];
+
 $(document).ready(readyNow);
 
 //main function for other functions & click events to go in to
@@ -25,11 +28,29 @@ annualSalary: $(`#annualSalaryIn`).val()
 }//end inputObject
 //console.log(inputObject);
 
-//add inputs to table row// need to figure out how to get the in right columns
-$(`#table`).append(`<tr><td>${inputObject.firstName}<td><tr>`);
-$(`#table`).append(`<tr><td>${inputObject.lastName}<td><tr>`);
-$(`#table`).append(`<tr><td>${inputObject.ID}<td><tr>`);
-$(`#table`).append(`<tr><td>${inputObject.title}<td><tr>`);
-$(`#table`).append(`<tr><td>${inputObject.annualSalary}<td><tr>`);
+//add to employeeList
+employeeList.push(inputObject);
+//console.log(employeeList);
+
+addToDom();
 
 }//end runSubmitClick
+
+function addToDom() {
+    //clear body so it doesn't exponentially add list
+    $('#table').empty();
+
+    for(let inputObject of employeeList) {
+        const row = $(`
+        <tr>
+            <td>${inputObject.firstName}</td>
+            <td>${inputObject.lastName}</td>
+            <td>${inputObject.ID}</td>
+            <td>${inputObject.title}</td>
+            <td>${inputObject.annualSalary}</td>
+            <td><button>Delete</button></td>
+        </tr>
+        `);
+        $('#table').append(row);
+    }//end for 
+}//end addToDom
